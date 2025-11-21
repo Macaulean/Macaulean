dc5f00d93216f889a42f290fd9bef82fa84b3154
Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
2025-11-21 14:49:24 -0500

    Update Macaulean/Serialize.lean
    Co-authored-by: Jay Yang <jkelleyy@gmail.com>

--- Macaulean/Serialize.lean
+++ Macaulean/Serialize.lean
@@ -6,7 +6,7 @@ namespace Lean.Grind.CommRing
 
 def Mon.serialize : Mon → Monomial
   | .unit => #[]
-  | .mult p m => m.serialize |>.push p
+  | .mult p m => m.serialize.push p
 
 def Poly.serialize : Poly → PolynomialData
   | .num k => #[⟨k, #[]⟩]
