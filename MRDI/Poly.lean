695f0967ecad671cf3455d46ecc6914d51fbc698
Jay Yang <jkelleyy@gmail.com>
2025-11-25 20:41:53 -0600

    Separate out the MRDI specific parts of the json format to a separate file

--- MRDI/Poly.lean
+++ MRDI/Poly.lean
@@ -1,17 +1,25 @@
 import Lean.Data.Json.FromToJson.Extra
+import MRDI.Basic
 
 open Lean Json
-
-def Lean.githubURL := "https://github.com/leanprover/lean4"
 
 namespace MRDI
 
 instance : ToJson Grind.CommRing.Power where
   toJson p := .arr #[p.x, p.k]
 
+instance : FromJson Grind.CommRing.Power where
+  fromJson? := fun
+    | .arr #[x, k] => do
+      let xnat ← x.getNat?
+      let knat ← k.getNat?
+      pure ⟨xnat, knat⟩
+    | _ => .error "Expected a pair of naturals"
+
 /-- An array of powers representing a monomial. -/
 def Monomial := Array Grind.CommRing.Power
 deriving instance ToJson for Monomial
+deriving instance FromJson for Monomial
 
 /-- A term in a polynomial, consisting of a coefficient and a monomial. -/
 structure Term where
@@ -21,23 +29,43 @@ structure Term where
 instance : ToJson Term where
   toJson t := .arr #[t.coeff, toJson t.mon]
 
+instance : FromJson Term where
+  fromJson? := fun
+    | .arr #[c, m] => do
+      let cnat ← c.getNat?
+      let mon ← fromJson? m
+      pure ⟨cnat, mon⟩
+    | _ => .error "Expected a pair of a coefficient and a monomial"
+
 /-- A polynomial represented as an array of terms. -/
 def PolynomialData := Array Term
 deriving instance ToJson for PolynomialData
+deriving instance FromJson for PolynomialData
 
 structure Poly where
   data : PolynomialData
 
-instance : ToJson Poly where
-  toJson p := .mkObj [("_ns", .mkObj [("Lean", .arr #[.str Lean.githubURL,
-                                               .str Lean.versionString])]),
-                      ("_type", .str "Lean.Grind.CommRing.Poly"),
-                      ("data", toJson p.data)]
+-- instance : ToJson Poly where
+--   toJson p := .mkObj [("_ns", .mkObj [("Lean", .arr #[.str Lean.githubURL,
+--                                                .str Lean.versionString])]),
+--                       ("_type", .str "Lean.Grind.CommRing.Poly"),
+--                       ("data", toJson p.data)]
 
--- def test : Poly := {
---   data := #[
---     { coeff := 3, mon := #[] },
---     { coeff := 5, mon := #[ ⟨2, 3⟩ ] }
---   ]
--- }
--- #eval toJson test
+instance : ToJson Poly where
+  toJson p := toJson p.data
+
+instance : FromJson Poly where
+  fromJson? x := do
+    let pdata ← fromJson? x
+    pure <| Poly.mk pdata
+
+instance : MrdiType Poly where
+  mrdiType := .string "Lean.Grind.CommRing.Poly"
+
+def test : Poly := {
+  data := #[
+    { coeff := 3, mon := #[] },
+    { coeff := 5, mon := #[ ⟨2, 3⟩ ] }
+  ]
+}
+#eval toJson <| toMrdi test
